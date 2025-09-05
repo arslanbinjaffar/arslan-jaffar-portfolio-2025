ac8e69e368aa23f2d4c611abe1233305b9e756d3
Arslan Jaffar <106986031+arslanbinjaffar@users.noreply.github.com>
2025-09-05 13:39:16 +0500

    refactor(projectsData): reorganize project entries for better maintainability
    The project entries were reordered to group related projects together and improve readability. No functional changes were made.

--- src/components/Projects/projectsData.js
+++ src/components/Projects/projectsData.js
@@ -17,15 +17,7 @@ const projects = [
     demoLink: "https://gigbase.io/",
     ghLink: "https://www.app.gigbase.io/auth/login",
   },
-  {
-    imgPath: require("../../Assets/Projects/eventbuizz mobile.png"),
-    isBlog: false,
-    title: "EventBuizz Mobile App",
-    description:
-      "Personal Chat Room or Workspace to share resources and hangout with friends built with react.js, Material-UI, and Firebase. Features include realtime messaging, image sharing, and support for message reactions.",
-    demoLink:
-      "https://play.google.com/store/apps/details?id=com.eventbuizz.app&hl=en&gl=US&pli=1",
-  },
+
   {
     imgPath: require("../../Assets/Projects/afiawebsite.png"),
     isBlog: false,
@@ -35,14 +27,7 @@ const projects = [
     demoLink: "https://www.aifaservices.ae/",
     ghLink: "https://github.com/Beyondevs/aifaWebsite",
   },
-  {
-    imgPath: require("../../Assets/Projects/eventbuizz.png"),
-    isBlog: false,
-    title: "Eventcenter Web-app",
-    description:
-      "I worked as a MERN developer on the Eventcenter project, a company product focused on managing event and program schedules. My responsibilities included developing and maintaining features for event timing, participant check-ins and check-outs, and other event-related functionalities. I ensured smooth integration between the frontend and backend while optimizing the system for performance and scalability. This project gave me valuable experience in handling real-time data, managing user interactions, and implementing secure, efficient workflows for event management.",
-    demoLink: "http://my.eventbuizz.com",
-  },
+ 
   {
     imgPath: require("../../Assets/Projects/aml-watcher.png"),
     isBlog: false,
@@ -51,33 +36,9 @@ const projects = [
       "I was currently working as a MERN developer on the AML Watcher application, a web solution owned by Enlatics Software Company. In this role, I contribute to the design, development, and maintenance of the application, focusing on leveraging the MERN stack to build a responsive, scalable, and secure platform. I collaborate with cross-functional teams to ensure seamless integration of front-end interfaces with back-end services, ensuring high performance and reliability. This project demonstrates my expertise in full-stack development, API design, and real-time data processing, aimed at providing a robust solution for anti-money laundering monitoring.",
     demoLink: "https://amlwatcher.com/",
   },
-  {
-    imgPath: require("../../Assets/Projects/ninja-training.png"),
-    isBlog: false,
-    title: "Ninja Training App",
-    description:
-      "Ninja Training App is a dynamic platform designed to help users train like a ninja. The app offers multiple training modules, each focused on different aspects of physical and mental strength, agility, and combat skills. Users can follow step-by-step workout routines, track their progress, and improve their skills through interactive exercise animations. Additionally, trainers can register to create their own training programs, allowing them to share their expertise with others. The app creates a unique experience for both trainees and trainers, making fitness fun, engaging, and accessible to all.",
-    demoLink: "https://ninja-training-alpha.vercel.app/",
-  },
-  {
-    imgPath:
-      "https://new-portfolio-one-murex.vercel.app/assets/schesti-30412cf1.png",
-    isBlog: false,
-    title: "Hasty App",
-    description:
-      "Hasty App is a comprehensive platform designed to streamline and manage real estate projects, focusing on the collaboration between contractors, subcontractors, and property owners. The app facilitates seamless project management, task coordination, and communication, helping all parties stay aligned throughout the construction process. Whether you're a contractor, subcontractor, or property owner, Hasty App provides the tools to ensure efficiency, transparency, and timely project delivery.",
-    ghLink: "https://github.com/arslanbinjaffar/hasty-next-app",
-  },
-  {
-    imgPath:
-      "https://new-portfolio-one-murex.vercel.app/assets/martialarts-006667b9.png",
-    isBlog: false,
-    title: "Martial Arts",
-    description:
-      "Martial Arts is a dynamic platform designed for martial arts trainers and enthusiasts. It features a seamless trainer sign-up and sign-in system, allowing instructors to easily create profiles and manage their sessions. The app also includes interactive animations for various training exercises, providing users with visual guides to help improve their technique and performance. Whether you're a trainer looking to engage with students or a practitioner aiming to refine your skills, this app combines functionality with a rich, user-friendly experience.",
-    ghLink: "https://github.com/arslanbinjaffar/martial-art-school",
-    demoLink: "https://martial-art-school-2.vercel.app/",
-  },
+  
+ 
+ 
   {
     imgPath: require("../../Assets/Projects/aifaERPsystem.png"),
     isBlog: false,
@@ -87,14 +48,7 @@ const projects = [
     demoLink: "https://staging.d31px19nsmiqd.amplifyapp.com/",
     ghLink: "https://github.com/Beyondevs/aifa-monorepo",
   },
-  {
-    imgPath:
-      "https://new-portfolio-one-murex.vercel.app/assets/fb-724d47b8.png",
-    isBlog: false,
-    title: "Facebook Clone",
-    description:
-      "Facebook Clone is a social media platform built with React.js, replicating the core features of Facebook. Users can sign up, log in, and post updates, images, and videos on their timeline. The app includes a dynamic feed, real-time notifications, and interactive comment and like functionalities, creating a seamless social experience. With a sleek and responsive design, this clone mimics the user interface and user experience of Facebook while showcasing the power of React for building modern web applications.",
-  },
+
   {
     imgPath: require("../../Assets/Projects/fleet.png"),
     isBlog: false,
@@ -140,8 +94,59 @@ const projects = [
       "Islamiyya Admin Panel provides administrative control over the platform’s operations, including user management, document and template control, system configuration, analytics, and support handling. Admins can oversee users, manage legal templates, configure system settings, track analytics, and respond to feedback, ensuring secure and efficient platform management.",
     demoLink: "https://admin.islamiyya.com/",
     ghLink: "https://github.com/Beyondevs/Islamiya-admin-panel-fe"
-  }
-
+  },
+  {
+    imgPath: require("../../Assets/Projects/eventbuizz mobile.png"),
+    isBlog: false,
+    title: "EventBuizz Mobile App",
+    description:
+      "Personal Chat Room or Workspace to share resources and hangout with friends built with react.js, Material-UI, and Firebase. Features include realtime messaging, image sharing, and support for message reactions.",
+    demoLink:
+      "https://play.google.com/store/apps/details?id=com.eventbuizz.app&hl=en&gl=US&pli=1",
+  },
+    {
+    imgPath:
+      "https://new-portfolio-one-murex.vercel.app/assets/fb-724d47b8.png",
+    isBlog: false,
+    title: "Facebook Clone",
+    description:
+      "Facebook Clone is a social media platform built with React.js, replicating the core features of Facebook. Users can sign up, log in, and post updates, images, and videos on their timeline. The app includes a dynamic feed, real-time notifications, and interactive comment and like functionalities, creating a seamless social experience. With a sleek and responsive design, this clone mimics the user interface and user experience of Facebook while showcasing the power of React for building modern web applications.",
+  },
+   {
+    imgPath:
+      "https://new-portfolio-one-murex.vercel.app/assets/martialarts-006667b9.png",
+    isBlog: false,
+    title: "Martial Arts",
+    description:
+      "Martial Arts is a dynamic platform designed for martial arts trainers and enthusiasts. It features a seamless trainer sign-up and sign-in system, allowing instructors to easily create profiles and manage their sessions. The app also includes interactive animations for various training exercises, providing users with visual guides to help improve their technique and performance. Whether you're a trainer looking to engage with students or a practitioner aiming to refine your skills, this app combines functionality with a rich, user-friendly experience.",
+    ghLink: "https://github.com/arslanbinjaffar/martial-art-school",
+    demoLink: "https://martial-art-school-2.vercel.app/",
+  },
+   {
+    imgPath:
+      "https://new-portfolio-one-murex.vercel.app/assets/schesti-30412cf1.png",
+    isBlog: false,
+    title: "Hasty App",
+    description:
+      "Hasty App is a comprehensive platform designed to streamline and manage real estate projects, focusing on the collaboration between contractors, subcontractors, and property owners. The app facilitates seamless project management, task coordination, and communication, helping all parties stay aligned throughout the construction process. Whether you're a contractor, subcontractor, or property owner, Hasty App provides the tools to ensure efficiency, transparency, and timely project delivery.",
+    ghLink: "https://github.com/arslanbinjaffar/hasty-next-app",
+  },
+  {
+    imgPath: require("../../Assets/Projects/ninja-training.png"),
+    isBlog: false,
+    title: "Ninja Training App",
+    description:
+      "Ninja Training App is a dynamic platform designed to help users train like a ninja. The app offers multiple training modules, each focused on different aspects of physical and mental strength, agility, and combat skills. Users can follow step-by-step workout routines, track their progress, and improve their skills through interactive exercise animations. Additionally, trainers can register to create their own training programs, allowing them to share their expertise with others. The app creates a unique experience for both trainees and trainers, making fitness fun, engaging, and accessible to all.",
+    demoLink: "https://ninja-training-alpha.vercel.app/",
+  },
+   {
+    imgPath: require("../../Assets/Projects/eventbuizz.png"),
+    isBlog: false,
+    title: "Eventcenter Web-app",
+    description:
+      "I worked as a MERN developer on the Eventcenter project, a company product focused on managing event and program schedules. My responsibilities included developing and maintaining features for event timing, participant check-ins and check-outs, and other event-related functionalities. I ensured smooth integration between the frontend and backend while optimizing the system for performance and scalability. This project gave me valuable experience in handling real-time data, managing user interactions, and implementing secure, efficient workflows for event management.",
+    demoLink: "http://my.eventbuizz.com",
+  },
 ];
 
 const backendProjects = [
